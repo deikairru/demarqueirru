f52df591b40ab384560ae938bc72500b4e2e231d
deikairru <deikairru@gmail.com>
2024-08-25 04:30:53 +0700

    chore: init project section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import Project from "@/components/Project";
 import Sidebar from "@/components/Sidebar";
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Project />
     </main>
   );
 }
