1fdb40c8b651641c8dcaf02d715eed1426172e26
deikairru <deikairru@gmail.com>
2024-08-26 22:58:44 +0700

    feat: add tape section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/sections/Hero";
 import Project from "@/components/sections/Project";
+import Tape from "@/components/sections/Tape";
 export default function Home() {
   return (
     <main>
       <Hero />
       <Project />
+      <Tape />
     </main>
   );
 }
