99b6c3e4fa301791df0786445b9ad57ebc6d6771
deikairru <deikairru@gmail.com>
2024-08-26 15:14:22 +0700

    refactor(svgr): changed svg to svgr

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -2,6 +2,9 @@
 
 import Image from "next/image";
 import React from "react";
+import HeroImage from "@/components/assets/icons/heroIcon.svg";
+import HandIcon from "@/components/assets/icons/handshake.svg";
+import ExploreIcon from "@/components/assets/icons/explore.svg";
 const Hero = () => {
   return (
     <section className="hero h-full pb-20 md:pb-32 xl:pb-0">
@@ -28,35 +31,18 @@ const Hero = () => {
             seamless and clean apps.
           </p>
           <div className="flex flex-col sm:flex-row gap-4 mt-8 justify-center xl:justify-start">
-            <button className="button-white font-medium flex flex-row gap-2 justify-center items-center text-xl [&>*:first-child]:hover:animate-spin sm:hover:scale-105 sm:origin-bottom-left duration-300">
-              <Image
-                src="/icons/explore.svg"
-                alt="handshake icon"
-                width={20}
-                height={20}
-              />
+            <button className="button-white font-medium flex flex-row gap-2 justify-center items-center text-xl [&>*:first-child]:hover:animate-spin hover:bg-slate-100 hover:text-slate-700 sm:hover:scale-105 sm:origin-bottom-left duration-300">
+              <ExploreIcon className="size-5" />
               <span>Explore Works</span>
             </button>
             <button className="button bg-violet-sky border-2 flex flex-row gap-2 justify-center items-center font-medium text-xl [&>*:first-child]:hover:animate-wiggle sm:hover:scale-105 sm:origin-bottom-right duration-300">
-              <Image
-                src="/icons/handshake.svg"
-                alt="handshake icon"
-                width={20}
-                height={20}
-                className="rotate-12"
-              />
+              <HandIcon alt="handshake icon" className="rotate-12 size-6" />
               <span>Lets Connect</span>
             </button>
           </div>
         </div>
 
-        <Image
-          src="/icons/heroIcon.svg"
-          alt="hero icon"
-          width={650}
-          height={650}
-          className="hidden xl:block mt-32 -translate-y-16 select-none"
-        />
+        <HeroImage className="hidden xl:block mt-32 -translate-y-16 select-none" />
       </div>
     </section>
   );
